c105ae7b6625667b7a391b6ddae300d16f66c114
Imperi13 <imperi.night@gmail.com>
2020-03-08 17:05:58 +0900

    make TaskAdd

---
+++ src/components/TodoList.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div>TodoList</div>
+    <div class="list">
+      <div class="done">
+        <div>Done!!!</div>
+        <ul>
+          <li v-for="task in doneLists" :key="task.name">
+            {{ task.name }}
+          </li>
+        </ul>
+      </div>
+      <div class="notyet">
+        <div>Not Yet...</div>
+        <ul>
+          <li v-for="task in yetLists" :key="task.name">
+            {{ task.name }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="input">
+      <label>
+        Add new task
+        <input type="text" v-model="newTaskName" />
+      </label>
+      <button @click="addTask">add!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      lists: [
+        { name: "Test", isdone: false },
+        { name: "test2", isdone: true }
+      ],
+      newTaskName: ""
+    };
+  },
+  computed: {
+    doneLists: function() {
+      return this.lists.filter(function(task) {
+        return task.isdone;
+      });
+    },
+    yetLists: function() {
+      return this.lists.filter(function(task) {
+        return !task.isdone;
+      });
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskName != "")
+        this.lists.push({ name: this.newTaskName, isdone: false });
+      this.newTaskName = "";
+    }
+  }
+};
+</script>
+
+<style>
+.list {
+  width: 100px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
